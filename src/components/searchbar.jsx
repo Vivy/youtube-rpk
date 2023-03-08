1e1c86d2d7ebe22e8f68ad7af46164cb7d35b515
Vivy <Tomitaviorel@yahoo.com>
2023-03-08 18:01:34 +0200

    feat:search working

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,11 +1,24 @@
 import { Search } from '@mui/icons-material';
 import { IconButton, Paper } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearachBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  };
   return (
     <Paper
       component='form'
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -17,8 +30,8 @@ const SearachBar = () => {
       <input
         className='search-bar'
         placeholder='Search..'
-        value=''
-        onChange={() => {}}
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton type='submit' sx={{ p: '10px', color: 'red' }}>
         <Search />
