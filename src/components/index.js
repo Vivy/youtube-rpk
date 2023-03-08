bf3cd90455967745cdca1fd9e6f60e03ca9fed03
Vivy <Tomitaviorel@yahoo.com>
2023-03-08 12:03:33 +0200

    creating laout

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Feed } from './feed';
+export { default as Navbar } from './navbar';
+export { default as ChannelDetail } from './channeldetail';
+export { default as SearchFeed } from './searchfeed';
+export { default as VideoDetail } from './videodetail';
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { logo } from '../utils/constants';
+
+const Navbar = () => {
+  return <Stack></Stack>;
+};
+
+export default Navbar;
